fa1fb99e7b54d507d8011ed16db682dff12f8b1e
the-muppet <jette@mtgban.com>
2024-02-16 01:13:52 -0600

    vectorized my masks

--- app/utils/masks.py
+++ app/utils/masks.py
@@ -1,54 +1,23 @@
-@staticmethod
-def is_not_empty(value: str | int | float) -> bool:
-    """
-    Checks if a given string value is not empty.
-    Args:
-        value (str): The value to check.
-    Returns:
-        bool: True if the value is not empty, False otherwise.
-    """
-    return value != ""
+import pandas as pd
 
 
-@staticmethod
-def is_numeric(value: int | float) -> bool:
-    """
-    Attempts to convert a string to a float and checks if it is a positive number.
-    Args:
-        value (Any): The value to convert and check.
-    Returns:
-        bool: True if conversion is successful and value is positive, False otherwise.
-    """
-    try:
-        val = float(value)
-        if val > 0:
-            return True
-        else:
-            return False
-    except ValueError:
-        return False
+def is_not_empty(series: pd.Series) -> pd.Series:
+    """Checks if each value in the Series is not empty."""
+    return series != ""
 
 
-@staticmethod
-def is_positive_integer(value: int | float) -> bool:
-    """
-    Checks if the input value can be converted to a positive integer.
-    """
-    try:
-        val = int(value)
-        return val > 0
-    except (ValueError, TypeError):
-        return False
+def is_positive_numeric(series: pd.Series) -> pd.Series:
+    """Checks if each value in the Series is a positive number."""
+    numeric_series = pd.to_numeric(series, errors="coerce")
+    return numeric_series > 0
 
 
-@staticmethod
-def is_valid_tcgplayer_id(value: int | str) -> bool:
-    """
-    Validates a TCGplayer ID by checking if it is numeric and not empty.
-    Args:
-        value (str): The TCGplayer ID to validate.
-    Returns:
-        bool: True if the ID is valid, False otherwise.
-    """
-    if is_numeric(value) and is_not_empty(value):
-        return True
+def is_valid_tcgplayer_id(series: pd.Series) -> pd.Series:
+    """Validates TCGplayer ID by checking if it is numeric and not empty."""
+    return is_positive_numeric(series) & is_not_empty(series)
+
+
+def is_positive_integer(series: pd.Series) -> pd.Series:
+    """Checks if each value in the Series can be converted to a positive integer."""
+    integer_series = pd.to_numeric(series, errors="coerce").fillna(0).astype(int)
+    return integer_series > 0
